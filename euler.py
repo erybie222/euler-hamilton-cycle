086d57fb788c3935eb4248dc9bab7408d40f9aac
Eryk Bielawski <erybie222@gmail.com>
2025-05-14 23:16:15 +0200

    euler cycle

---
+++ euler.py
@@ -0,0 +1,19 @@
+def find_euler_cycle(graph: dict[int , set(int)]) -> list[int]:
+    g = {u: set(neigh) for u, neigh in graph.items()}
+
+    stack = []
+    cycle = []
+
+    start = next(iter(g))
+    stack.append(start)
+
+    while stack:
+        v= stack[-1]
+        if g[v]:
+            u = g[v].pop()
+            g[u].remove(v)
+            stack.append(u)
+        else:
+            cycle.append(stack.pop())
+
+    return cycle[::-1]
